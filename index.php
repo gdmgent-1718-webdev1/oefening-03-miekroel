123a610b70b02b60d6bf97df94cc0742cde104c6
miekroel <mieke.roelandt@student.arteveldehs.be>
2017-10-08 21:25:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$x = 10;
+$y = 7;
+
+echo "som: ", $x + $y, "<br>";
+echo "verschil: ", $x - $y, "<br>";
+echo "vermenigvuldiging: ", $x * $y, "<br>";
+echo "deling: ", $x / $y, "<br>";
+echo "modulo: ", $x % $y;
+
+
+?>
+    
+</body>
+</html>
